e29c2d91354538c88414a8f04d48771ee7115836
SimonEvs <simonevseev07@mail.ru>
2022-09-18 16:54:53 +0300

    ADD

--- HelloPython/ThirdLesson/tasks/tasks_in_HW.py
+++ HelloPython/ThirdLesson/tasks/tasks_in_HW.py
@@ -57,79 +57,95 @@
 
 # 3.Создайте программу для игры в ""Крестики-нолики"".
 
-def Number_KN(n):
-    print('Пример расположения ')
-    D = '1 2 3'
-    E = '4 5 6'
-    F = '7 8 9'
-    print(D)
-    print(E)
-    print(F)
-    print()
+# def Number_KN(n):
+#     print('Пример расположения ')
+#     D = '1 2 3'
+#     E = '4 5 6'
+#     F = '7 8 9'
+#     print(D)
+#     print(E)
+#     print(F)
+#     print()
 
-def Print_ABC(A,B,C):
+# def Print_ABC(A,B,C):
+#     print(A)
+#     print(B)
+#     print(C)
+from os import system
+
+A = ['-', '-', '-']
+B = ['-', '-', '-']
+C = ['-', '-', '-']
+
+for i in range(0, 4):
+    # def First_GO(A,B,C):
+    first = int(input('Введите переменную '))
+    system("cls")
+# if A[ !=('O') and first!=('X'):
+    if (first == 1) and (A[0] != ('O')) and (A[0] != ('X')):
+        A[0] = 'X'
+    if first == 2 and A[1] != ('O') and A[1] != ('X'):
+        A[1] = 'X'
+    if first == 3 and A[2] != ('O') and A[2] != ('X'):
+        A[2] = 'X'
+    if first == 4 and B[0] != ('O') and B[0] != ('X'):
+        B[0] = 'X'
+    if first == 5 and B[1] != ('O') and B[1] != ('X'):
+        B[1] = 'X'
+    if first == 6 and B[2] != ('O') and B[2] != ('X'):
+        B[2] = 'X'
+    if first == 7 and C[0] != ('O') and C[0] != ('X'):
+        C[0] = 'X'
+    if first == 8 and C[1] != ('O') and C[1] != ('X'):
+        C[1] = 'X'
+    if first == 9 and C[2] != ('O') and C[2] != ('X'):
+        C[2] = 'X'
+    print(A)
+    print(B)
+    print(C)
+    
+# else:
+#     print('Позиция занята!')
+    # return(First_GO(A,B,C))
+# Print_ABC(A,B,C)
+
+# def Second_GO(A,B,C):
+    second = int(input('Введите переменную '))
+    system("cls")
+# if second !=('X') and second !=('O'):
+    if second == 1 and (A[0] != ('O')) and (A[0] != ('X')):
+        A[0] = 'O'
+    if second == 2 and A[1] != ('O') and A[1] != ('X'):
+        A[1] = 'O'
+    if second == 3 and A[2] != ('O') and A[2] != ('X'):
+        A[2] = 'O'
+    if second == 4 and B[0] != ('O') and B[0] != ('X'):
+        B[0] = 'O'
+    if second == 5 and B[1] != ('O') and B[1] != ('X'):
+        B[1] = 'O'
+    if second == 6 and B[2] != ('O') and B[2] != ('X'):
+        B[2] = 'O'
+    if second == 7 and C[0] != ('O') and C[0] != ('X'):
+        C[0] = 'O'
+    if second == 8 and C[1] != ('O') and C[1] != ('X'):
+        C[1] = 'O'
+    if second == 9 and C[2] != ('O') and C[2] != ('X'):
+        C[2] = 'O'
     print(A)
     print(B)
     print(C)
 
-def First_GO(k):
-    first = input('Введите позицию для X ')
-    if first !=('O') and first!=('X'):
-        if first == 1:
-            A[0] = 'X'
-        if first == 2:
-            A[2] = 'X'
-        if first == 3:
-            A[4] = 'X'
-        if first == 4:
-            B[0] = 'X'
-        if first == 5:
-            B[2] = 'X'
-        if first == 6:
-            B[4] = 'X'
-        if first == 7:
-            C[0] = 'X'
-        if first == 8:
-            C[2] = 'X'
-        if first == 9:
-            C[4] = 'X'
-    else:
-        print('Позиция занята!')
-        return(k)
-    Print_ABC(A,B,C)
+    if (A[0]==B[0]==C[0]) or (A[1]==B[1]==C[1] or (A[2]==B[2]==C[2]) or (A[0]==A[1]==A[2]) or (A[0]==A[1]==A[2]) or (B[0]==B[1]==B[2]) or (C[0]==C[1]==C[2]) or (A[0]==B[1]==C[2]) or (A[2]==B[1]==C[0])):
+        print("Игра окончена")
+# else:
+#         print('Позиция занята!')
+#         # return(Second_GO(A,B,C))
 
-def Second_GO(k):
-    second = input('Введите позицию для О ')
-    if second !=('X') and second !=('O'):
-        if second == 1:
-            A[0] = input('O')
-        if second == 2:
-            A[2] = input('O')
-        if second == 3:
-            A[4] = input('O')
-        if second == 4:
-            B[0] = input('O')
-        if second == 5:
-            B[2] = input('O')
-        if second == 6:
-            B[4] = input('O')
-        if second == 7:
-            C[0] = input('O')
-        if second == 8:
-            C[2] = input('O')
-        if second == 9:
-            C[4] = input('O')
-    else:
-            print('Позиция занята!')
-            return(k)
 
-A = ('- - -')
-B = ('- - -')
-C = ('- - -')
-
-First_GO(1)
-Second_GO(1)
-Print_ABC(1)
+# for i in range(0,9):
+#     First_GO(A,B,C)
+#     Second_GO(A,B,C)
+#     Print_ABC(A,B,C)
 
 
 # 4.Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных.
