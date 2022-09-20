5e62cd1bf17138976616838809f45abb292d40de
SimonEvs <simonevseev07@mail.ru>
2022-09-20 19:09:06 +0300

    ADD

--- HelloPython/ThirdLesson/tasks/tasks_in_HW2.py
+++ HelloPython/ThirdLesson/tasks/tasks_in_HW2.py
@@ -2,31 +2,33 @@
 
 
 
-string = ('1+2*3+2').split()
-for i in range(len(string)):
-    if string[i].isdigit():
-        string[i] = int(string[i])
-op_1 = {'+': lambda x, y: x+y, '-': lambda x, y: x-y}
-op_2 = {'*': lambda x, y: x*y, '/': lambda x, y: x/y}
+new_string = '1 + 2 * 3 + 2'.split()
+for i in range(len(new_string)):
+    if new_string[i].isdigit():
+        new_string[i] = int(new_string[i])
+op_1 = {'+': lambda x, y: x+y, 
+'-': lambda x, y: x-y}
+op_2 = {'*': lambda x, y: x*y, 
+'/': lambda x, y: x/y}
 
 index = 0
-while '*' in string or '/' in string:
-    if string[index] in op_2:
-        temp = op_2[string[index]](string[index-1], string[index+1])
-        del string[index-1:index+2]
-        string.insert(index-1, temp)
+while '*' in new_string or '/' in new_string:
+    if new_string[index] in op_2:
+        temp = op_2[new_string[index]](new_string[index-1], new_string[index+1])
+        del new_string[index-1:index+2]
+        new_string.insert(index-1, temp)
         index = 0
     else:
         index += 1
 
-while '+' in string or '-' in string:
-    if string[index] in op_1:
-        temp = op_1[string[index]](string[index-1], string[index+1])
-        del string[index-1:index+2]
-        string.insert(index-1, temp)
+while '+' in new_string or '-' in new_string:
+    if new_string[index] in op_1:
+        temp = op_1[new_string[index]](new_string[index-1], new_string[index+1])
+        del new_string[index-1:index+2]
+        new_string.insert(index-1, temp)
         index = 0
     else:
         index += 1
-print(*string)
+print(new_string)
 
 print(eval('1+2*3+2'))
