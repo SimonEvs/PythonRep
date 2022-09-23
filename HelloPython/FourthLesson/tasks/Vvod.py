b5cc1577285107eb4ce9bea81a1e253d3ef82549
SimonEvs <simonevseev07@mail.ru>
2022-09-23 18:03:40 +0300

    ADD

---
+++ HelloPython/FourthLesson/tasks/Vvod.py
@@ -0,0 +1,11 @@
+
+
+
+def Input_Data():
+    name = input('Введите Имя: ')
+    surname = input('Введите Фамилию: ')
+    phone_number = input('Введите номер телефона: ')
+    other_inf = input('Введите описание: ')
+    print(f'{name}, {surname}, {phone_number}, {other_inf}')
+
+# Input_Data()
