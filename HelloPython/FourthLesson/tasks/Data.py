5674f3118ae706e8cba52246491acc4662ab8358
SimonEvs <simonevseev07@mail.ru>
2022-09-23 18:50:57 +0300

    ADD

---
+++ HelloPython/FourthLesson/tasks/Data.py
@@ -0,0 +1,28 @@
+
+
+
+def Input_Data():
+    name = input('Введите Имя: ')
+    surname = input('Введите Фамилию: ')
+    phone_number = input('Введите номер телефона: ')
+    other_inf = input('Введите описание: ')
+    print(f'{name}, {surname}, {phone_number}, {other_inf}')
+
+# Input_Data()
+
+
+def get_Name(data):
+    name=input('введите Имя: ')
+    return name
+
+def get_surname(data):
+    surname=input('Введите фамилию: ')
+    return surname
+
+def get_phone_number(data):
+    phone=('Введите номер телефона: ')
+    return phone
+
+def get_other(data):
+    other=('Введите остальное: ')
+    return other
--- HelloPython/FourthLesson/tasks/recording.py
+++ HelloPython/FourthLesson/tasks/recording.py
@@ -1,2 +1,28 @@
 
-path = 'Phone_numbersfolder.txt'
+# path = 'Phone_numbersfolder.txt'
+data=567
+
+def First_name(data):
+    with open('Phone_number_folder.csv','a') as file:
+        file.write('Name;{}\n'
+                        .format(data))
+
+
+def Second_name(data):
+    with open('Phone_number_folder.csv','a') as file:
+        file.write('Surname;{}\n'
+                        .format(data))
+
+
+def Phone_number(data):
+    with open('Phone_number_folder.csv','a') as file:
+        file.write('Phone number;{}\n'
+                        .format(data))
+
+def Other_information(data):
+    with open('Phone_number_folder.csv','a') as file:
+        file.write('Other;{}\n'
+                        .format(data))
+
+First_name(data)
+Second_name(data)
