117c40fb98e8128a4eb32e8998c66e7133e7ce2c
SimonEvs <simonevseev07@mail.ru>
2022-09-27 19:22:45 +0300

    ADD

--- HelloPython/FourthLesson/tasks/main.py
+++ HelloPython/FourthLesson/tasks/main.py
@@ -1,3 +1,14 @@
+from os import system
+system("cls")
 import modelist
 
-modelist.Buttom_Finish()
+# modelist.Print_Finish()
+n=input('Что вы хотите сделать?\n Чтобы записать в книгу нажмите 1.\n \
+Чтобы открыть для прочтения нажмите 2.\n Введите цифру: ')
+    
+if n=='1':
+    modelist.Buttom_Finish()
+elif n=='2':
+    modelist.Console_Read()
+else:
+    print('Вы ввели не корректные данные!')
--- HelloPython/FourthLesson/tasks/modelist.py
+++ HelloPython/FourthLesson/tasks/modelist.py
@@ -1,10 +1,29 @@
 import Data
 import recording
-
+# from os import system
+# system("cls")
 def Buttom_Finish():
     recording.First_name(Data.get_Name())
     recording.Second_name(Data.get_surname())
     recording.Phone_number(Data.get_phone_number())
     recording.Other_information(Data.get_other())
 
-# Finish()
+    
+def Console_Read():
+    path ='Phone_number_folder.csv'
+    data=open(path,'r')
+    for line in data:
+        print(line)
+    data.close()
+
+# def Print_Finish():
+#     input('Что вы хотите сделать?\n Чтобы записать в книгу нажмите 1.\n Чтобы открыть для прочтения нажмите 2.\n Введите цифру: '))
+#     n=input()
+#     if n==1:
+#         Buttom_Finish()
+#     elif n==2:
+#         Console_Read()
+#     else:
+#         print('Вы ввели не корректные данные!')
+#         Print_Finish()
+        
