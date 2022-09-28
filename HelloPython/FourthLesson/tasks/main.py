22b2529e44b7e1a68eb48d33b5bf15235631cbc9
SimonEvs <simonevseev07@mail.ru>
2022-09-28 18:50:17 +0300

    ADD main changes

--- HelloPython/FourthLesson/tasks/main.py
+++ HelloPython/FourthLesson/tasks/main.py
@@ -1,11 +1,11 @@
 # from html_generator import html_create
 # import xml_generator as xm
 # from xml_generator import create1
-# import premain 
+import premain 
 import all_generator
 
-# premain.Oblozka()
+premain.Oblozka()
 # print(html_create())
 # print(create1())
 
-all_generator.Finish_prog()
+# all_generator.Finish_prog()
--- HelloPython/FourthLesson/tasks/premain.py
+++ HelloPython/FourthLesson/tasks/premain.py
@@ -1,14 +1,16 @@
 from os import system
 import modelist
+import all_generator
 system("cls")
 def Oblozka():
     n=input('Что вы хотите сделать?\n Чтобы записать в книгу нажмите 1.\n \
     Чтобы открыть для прочтения нажмите 2.\n Введите цифру: ')
         
     if n=='1':
-        modelist.Buttom_Finish()
+        all_generator.Finish_prog()
     elif n=='2':
         modelist.Console_Read()
     else:
+        system("cls")
         print('Вы ввели не корректные данные!')
         Oblozka()
